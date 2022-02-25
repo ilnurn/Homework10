31a117265e89140c85a40ea6320b4fc4d65da567
Ilnur <nabiulli@mail.ru>
2022-02-26 02:32:21 +0500

    Решил домашнее задание по теме Методы объектов

---
+++ src/ru/skypro/Author.java
@@ -0,0 +1,15 @@
+package ru.skypro;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return this.firstName + this.lastName;
+    }
+}
